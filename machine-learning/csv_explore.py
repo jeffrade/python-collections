75113e8144a2ab6e43e6fe8b95f3894d57c7f387
jeffrade <jrade@cars.com>
2019-12-23 16:04:05 -0600

    Adding column header and unique column values features via pandas

--- machine-learning/csv_explore.py
+++ machine-learning/csv_explore.py
@@ -1,29 +1,44 @@
 #!/usr/bin/env python3
 # Script that uses pandas lib to understand data in a csv file.
+# Usage: ./csv_explore.py <file> <command> [<options>...]
 import sys
 import pandas as pandas
 
 def main(args):
-    print("Starting...")
     e = CsvExplore(args)
     e.exec()
-    print("Done")
 
 class CsvExplore():
     filename = None
+    command = None
     column = None
 
     def __init__(self, args):
-        if len(args) == 2:
+        if len(args) == 3:
             self.filename = args[0]
-            self.column = args[1]
+            self.command = args[1]
+            self.column = args[2]
+        elif len(args) == 2:
+            self.filename = args[0]
+            self.command = args[1]
         else:
-            sys.exit("You must supply a file and column name as args.")
+            sys.exit("You must supply args: \"file command [options]\"")
     
     def exec(self):
-        print(self.filename)
+        if self.command == 'uniq':
+            self.exec_uniq()
+        elif self.command == 'headers':
+            self.exec_headers()
+        
+    
+    def exec_uniq(self):
         d = pandas.read_csv(self.filename)
         print(d[self.column].unique())
+
+    def exec_headers(self):
+        d = pandas.read_csv(self.filename)
+        print(d.columns.values.tolist())
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
